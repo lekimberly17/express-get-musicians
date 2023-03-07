706d11ba20edb74ea454058be0f2e5801bc13d50
lekimberly17 <lekimberly509@gmail.com>
2023-03-07 13:48:37 -0800

    updates

--- server.js
+++ server.js
@@ -6,6 +6,44 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+
+// middleware
+app.use(express.json())
+
+// Create an express route for creating (adding) a new Musician on your musician database. 
+app.post('/musician', async (req, res) => {
+    const { name, instrument} = req.body;
+    const musician = await Musician.create({
+        name,
+        instrument
+    })
+    res.json(newMusician)
+})
+
+// Create an express route for updating (replacing) an existing musician with a new musician on your musician database based on ID.
+app.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, instrument} = req.body
+    const musician = await Musician.findByPk(id)
+
+  
+    musician.name = name
+    musician.instrument = instrument
+
+    const updatedMusician = await musician.save()
+
+    res.json(updatedMusician)
+})
+
+// Create an express route for deleting (removing) a musician on your database. 
+app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const musician = await Musician.findByPk(id);
+
+    await musician.destroy();
+    res.json({ message: 'Restaurant deleted successfully' });
+})
+
 app.get('/musicians', async (req, res) => {
     const data = await Musician.findAll()
     res.send(data);
